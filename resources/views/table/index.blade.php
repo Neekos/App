a93a64d72b249e7d334ecf68482792a0813eed0a
Андрей Спиридонов <and1997310366@mail.ru>
2018-12-03 16:24:55 +0700

    Сcылки
    Сылки на id

--- resources/views/table/index.blade.php
+++ resources/views/table/index.blade.php
@@ -6,7 +6,9 @@
 <body>
 	<H1>Таблица</H1>
 	@foreach ($table as $value)
-		<li>{{ $value->body }}</li>
+		<li>
+			<a href="table/{{ $value->id }}">{{ $value->body }}</a>
+		</li>
 	@endforeach
 </body>
 </html>
