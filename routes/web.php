587190b14572849ca5aa1e3e84ddb0cb8bf46c8b
Андрей Спиридонов <and1997310366@mail.ru>
2018-12-04 18:14:37 +0700

    MVC
    Разработка с помощью моделей и контроллеров

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,18 @@ Route::get('hello', function () {
     return view('hello', compact('world'));/*Объявление массивов*/
 });
 
-Route::get('table', function () {
-	$table = DB::table('mytable')->get();//Получить данные из бд
-    return view('table.index', compact('table'));
-});
+// Route::get('table', function () {
+// 	$table = App\TableTest::incomplete();
+// 	// $table = DB::table('mytable')->get();//Получить данные из бд
+//     return view('table.index', compact('table'));
+// });
 
-Route::get('table/{num}', function ($id) {
-	$num = DB::table('mytable')->find($id);//Получить данные из бд по id
-    //dd($num);//dump and dump
-    return view('table.show', compact('num'));
-});
+// Route::get('table/{num}', function ($id) {
+// 	// $num = DB::table('mytable')->find($id);//Получить данные из бд по id
+//     //dd($num);//dump and dump
+//     $num = App\TableTest::find($id);
+//     return view('table.show', compact('num'));
+// });
+
+Route::get('table', 'TableTestController@index');// Обращение к методам через контроллеры
+Route::get('table/{num}', 'TableTestController@show');
